c9869083dd181cce97f9afd38d01143b61926e4b
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-02-09 08:58:10 +0100

    pk

--- src/_helpers/AuthGuard.js
+++ src/_helpers/AuthGuard.js
@@ -1,14 +1,9 @@
 import { Navigate } from "react-router-dom";
 import { accountService } from "@/_services/account.service";
-// import { createBrowserHistory } from "history";
-
-// const hist = createBrowserHistory();
 
 const AuthGuard = ({ children }) => {
   if (!accountService.isLoggedIn()) {
     if (localStorage.getItem("username") == null) {
-      // hist.push("/auth");
-
       return <Navigate to="/auth" />;
     }
   }
