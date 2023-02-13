f9a140c91727c2522f5c64ae55bfb4fdeb9fb966
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-02-13 14:47:17 +0100

    css

--- src/admin/components/pokemons/PokemonList.js
+++ src/admin/components/pokemons/PokemonList.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 // Imported Functions ====>
 import { pokemonService } from "@/_services";
 import { AiFillHeart } from "react-icons/ai";
+import "./pokemon.css";
 
 const PokemonList = () => {
   let navigate = useNavigate();
@@ -20,23 +21,27 @@ const PokemonList = () => {
   }
   return (
     <div className="mainContent grid">
-      PokemonList
-      <div className="secContainer flex">
-        {data.data.map((pokemon) => (
-          <div
-            onClick={(e) => navigate(`../pokemon/edit/${pokemon.id}`)}
-            key={pokemon.id}
-            className="singleItem"
-          >
-            <AiFillHeart className="icon" />
-            <img src={pokemon.picture} alt={pokemon.name} />
-            <h2>{pokemon.name}</h2>
-            <h2>{pokemon.cp}</h2>
-            <h2>{pokemon.hp}</h2>
-            <h3>{pokemon.types.join(", ")}</h3>
-            <h2>{pokemon.created}</h2>
-          </div>
-        ))}
+      <div className="carousel">
+        PokemonList
+        <div className="secContainer flex">
+          {data.data.map((pokemon) => (
+            <div className="slide">
+              <div
+                onClick={() => navigate(`../pokemon/edit/${pokemon.id}`)}
+                key={pokemon.id}
+                className="singleItem"
+              >
+                <AiFillHeart className="icon" />
+                <img src={pokemon.picture} alt={pokemon.name} />
+                <h2>{pokemon.name}</h2>
+                <h2>{pokemon.cp}</h2>
+                <h2>{pokemon.hp}</h2>
+                <h3>{pokemon.types.join(", ")}</h3>
+                <h2>{pokemon.created}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
